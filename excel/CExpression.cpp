a7d4468964a4957d1fb01aa8b27920c704847663
Bekzhan Omirzak <omirzbek@fit.cvut.cz>
2024-05-04 12:25:17 +0200

    added CExpression

---
+++ excel/CExpression.cpp
@@ -0,0 +1,2 @@
+#include "CExpression.h"
+
---
+++ excel/CExpression.h
@@ -0,0 +1,30 @@
+
+class CExpression {
+
+public:
+
+private:
+
+};
+
+class COperator : public CExpression {
+
+public:
+
+private:
+
+};
+
+class CPosition : public CExpression {
+public:
+
+private:
+
+};
+
+class CDouble : public CExpression {
+public:
+
+private:
+
+};
