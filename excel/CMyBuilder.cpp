7a4043333296f9579ca460ce54f69128a8020ed4
Bekzhan Omirzak <omirzbek@fit.cvut.cz>
2024-05-07 18:19:40 +0200

    implemented CMyBuilder methods

--- excel/CMyBuilder.cpp
+++ excel/CMyBuilder.cpp
@@ -1,5 +1,116 @@
-//
-// Created by Bekzhan Omirzak on 04.05.2024.
-//
-
+#include <iostream>
+#include <string>
 #include "CMyBuilder.h"
+
+CMyBuilder::CMyBuilder(){
+}
+
+void CMyBuilder::opAdd() {
+    std::cout << " + " << std::endl;
+    COperator toPush ( "+" );
+    ptrToPos ->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opDiv() {
+    std::cout << " / " << std::endl;
+    COperator toPush ( "/" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opEq() {
+    std::cout << " = " << std::endl;
+    COperator toPush ( "==" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opGe() {
+    std::cout << " ge " << std::endl;
+    COperator toPush ( ">=" ); // >=
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::valNumber ( double val ) {
+    //std::cout << " the number is " << val << std::endl;
+    CDouble toPush ( val );
+    ptrToPos->emplace_back ( toPush.clone() );
+    std::cout << "tmp second is ";
+    (* ( *ptrToPos )[0] ).print();
+    std::cout << std::endl;
+}
+
+void CMyBuilder::valString ( std::string val ) {
+    CString toPush ( val );
+    ptrToPos->emplace_back( toPush.clone() );
+    std::cout << "tmp second is ";
+    (* ( *ptrToPos )[0] ).print();
+    std::cout << " the string is " << val << std::endl;
+}
+
+void CMyBuilder::valReference ( std::string val ) {
+    CPos tmp ( val );
+    CPosition toPush ( tmp );
+    ptrToPos->emplace_back( toPush.clone() );
+    std::cout << "tmp second is ";
+    (* ( *ptrToPos )[0] ).print();
+    std::cout << " our position is " << val << std::endl;
+}
+
+void CMyBuilder::opGt() {
+    COperator toPush ( ">" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::valRange ( std::string val ) {
+
+}
+
+void CMyBuilder::opLe() {
+    COperator toPush ( "<=" );// <=
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opLt() {
+    COperator toPush ( "<" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opMul() {
+    COperator toPush ( "*" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opNe() {
+    COperator toPush ( "!=" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opNeg() {
+    COperator toPush ( "unar" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opPow() {
+    COperator toPush ( "^" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::opSub() {
+    COperator toPush ( "-" );
+    ptrToPos->emplace_back( toPush.clone() );
+}
+
+void CMyBuilder::funcCall ( std::string fnName, int paramCount ) {
+
+}
+
+void CMyBuilder::setPtr ( std::shared_ptr<std::vector<std::shared_ptr<CExpression>>> & ptr ) {
+    ptrToPos = ptr;
+}
+
+void CMyBuilder::printDOuble() {
+    //std::cout << std::endl;
+}
+
+void CMyBuilder::setTable ( std::unordered_map <std::string , std::vector<std::shared_ptr<CExpression>>> & data ) {
+    m_table = data;
+}
--- excel/CMyBuilder.h
+++ excel/CMyBuilder.h
@@ -1,16 +1,66 @@
-//
-// Created by Bekzhan Omirzak on 04.05.2024.
-//
-
 #ifndef EXCEL_CMYBUILDER_H
 #define EXCEL_CMYBUILDER_H
+#include <string>
+#include <vector>
 
+//x#include "CSpreadsheet.h"
 
+#include "expression.h"
+#include "CPos.h"
+#include "CExpression.h"
 
-class CMyBuilder {
+class CMyBuilder : public CExprBuilder {
+private:
+//    CPos position;
+//    std::shared_ptr<std::unordered_map <std::string , std::vector<std::shared_ptr<CExpression>>>> ptrToTable;
+    std::shared_ptr<std::vector<std::shared_ptr<CExpression>>> ptrToPos;
+    std::unordered_map <std::string , std::vector<std::shared_ptr<CExpression>>> m_table;
+    CPos m_pos;
+public:
 
+    CMyBuilder();
+
+    virtual void opAdd() override;
+
+    virtual void opSub() override;
+
+    virtual void opMul() override;
+
+    virtual void opDiv() override;
+
+    virtual void opPow() override;
+
+    virtual void opNeg() override;
+
+    virtual void opEq() override;
+
+    virtual void opNe() override;
+
+    virtual void opLt() override;
+
+    virtual void opLe() override;
+
+    virtual void opGt() override;
+
+    virtual void opGe() override;
+
+    virtual void valNumber ( double val ) override;
+
+    virtual void valString ( std::string val ) override;
+
+    virtual void valReference ( std::string val ) override;
+
+    virtual void valRange ( std::string val ) override;
+
+    virtual void funcCall ( std::string fnName,
+                            int paramCount ) override;
+
+    void printDOuble ();
+
+    void setPtr ( std::shared_ptr<std::vector<std::shared_ptr<CExpression>>> & ptr );
+
+    void setTable ( std::unordered_map <std::string , std::vector<std::shared_ptr<CExpression>>> & data );
 };
 
 
-
-#endif //EXCEL_CMYBUILDER_H
+#endif EXCEL_CMYBUILDER_H
