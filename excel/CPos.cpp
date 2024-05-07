8ceec35035db6577206b8b429d5cb0aa052e204e
Bekzhan Omirzak <omirzbek@fit.cvut.cz>
2024-05-07 18:20:04 +0200

    implemented CPos methods

--- excel/CPos.cpp
+++ excel/CPos.cpp
@@ -1,27 +1,88 @@
 #include "CPos.h"
+#include <iostream>
 #include <string>
+
+void CPos::toInt ( const std::string & pos , std::pair<long long int , int> & posInt ) {
+    int size = pos.size();
+    posInt.first = 0;
+    for ( int i = size - 1 , j = 0; i >= 0; i-- , j++ ) {
+        posInt.first += ( ( pos [ j ] - 'A' + 1  ) * pow ( 26 , i ) );
+        std::cout << posInt.first << std::endl;
+    }
+
+    std::cout << "end" << std::endl;
+}
+
+std::string CPos ::toStr ( long long int posInt ) {
+    std::string pos = "";
+    while ( posInt > 0 ) {
+        int remainder = posInt % 26;
+        char digit = 'A' + remainder - 1; // ('A'=1, 'B'=2, ..., 'Z'=26)
+        pos = digit + pos;
+        posInt /= 26;
+    }
+    std::cout << "decoded : " << pos << std::endl;
+    return pos;
+}
 
 CPos::CPos ( std::string_view str ) {
 
+    m_fixedColumn = false;
+    m_fixedRow = false;
     auto it = str.begin();
+    if ( * it == '$') {
+        m_fixedColumn = true;
+        it++;//skipping
+    }
     while ( std::isalpha ( *it ) ) {
-        it++;
         m_str.push_back ( ::toupper( *it ) );
+        it++;
     }
 
-    if ( it == str.begin() || it == str.end() || ! std::isdigit ( *it ) )
+    if ( it == str.begin() || it == str.end() )
         throw std::invalid_argument ( "format of position isn't right" );//if we haven't moved from the beginning means that first element isn't a letter
 
+    toInt ( m_str , m_int );
     auto it2 = it;
-    while ( std::isdigit ( *it ) && it != str.end() ) {
+    if ( * it == '$' ) {
+        m_fixedRow = true;
         it++;
-        m_str.push_back( *it );
     }
+
+    std::string tmp;
+    while ( std::isdigit ( *it ) && it != str.end() ) {
+        m_str.push_back( *it );
+        tmp.push_back( * it );
+        it++;
+    }
+    m_int.second = std::stoi( tmp );
 
     if ( it == it2 || it != str.end() )
         throw std::invalid_argument ( "format of position isn't right" );//if we haven't moved from the beginning
+
+
 }
 
-std::string CPos::getPos() const {
+std::string CPos::getPosStr() const {
     return m_str;
+}
+
+std::pair<long long int , int> CPos::getPosInt() const {
+    return m_int;
+}
+
+bool CPos::isFixedColumn() const {
+    return m_fixedColumn;
+}
+
+bool CPos::isFixedRow() const {
+    return m_fixedRow;
+}
+
+void CPos::setFixedColumn ( bool toSet ) {
+    m_fixedColumn = toSet;
+}
+
+void CPos::setVixedRow ( bool toSet ) {
+    m_fixedRow = toSet;
 }
--- excel/CPos.h
+++ excel/CPos.h
@@ -1,11 +1,26 @@
-#pragma once
 #include <string>
+#ifndef CPOS_H
+#define CPOS_H
 
 class CPos {
 public:
+    CPos() = default;
     CPos ( std::string_view str );
-    std::string  getPos () const;
+    void toInt ( const std::string & pos , std::pair<long long int , int> & posInt );
+    std::string toStr ( long long int posInt );
+    std::string  getPosStr () const;
+    std::pair<long long int , int> getPosInt () const;
+    bool isFixedColumn() const;
+    bool isFixedRow() const;
+    void setFixedColumn ( bool toSet );
+    void setVixedRow ( bool toSet );
 
 private:
     std::string m_str;
+    bool m_fixedColumn;
+    bool m_fixedRow;
+    std::pair< long long int , int> m_int;
 };
+
+
+#endif CPOS_H
