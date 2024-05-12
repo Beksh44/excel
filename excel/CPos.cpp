fce3549a257516369d6d0afec89b3530b0a05dcd
Bekzhan Omirzak <omirzbek@fit.cvut.cz>
2024-05-12 23:44:24 +0200

    final class of CPos

--- excel/CPos.cpp
+++ excel/CPos.cpp
@@ -2,16 +2,25 @@
 #include <iostream>
 #include <string>
 
+
+/*
+ * Convert a string to an integer
+ * @param pos - string
+ * @param posInt - pair of integers
+ */
+
 void CPos::toInt ( const std::string & pos , std::pair<long long int , int> & posInt ) {
     int size = pos.size();
     posInt.first = 0;
-    for ( int i = size - 1 , j = 0; i >= 0; i-- , j++ ) {
+    for ( int i = size - 1 , j = 0; i >= 0; i-- , j++ ) //convert the string to an integer
         posInt.first += ( ( pos [ j ] - 'A' + 1  ) * pow ( 26 , i ) );
-        std::cout << posInt.first << std::endl;
-    }
-
-    std::cout << "end" << std::endl;
 }
+
+/*
+ * Convert integer to a string
+ * @param posInt - integer
+ * @return the string
+ */
 
 std::string CPos ::toStr ( long long int posInt ) {
     std::string pos = "";
@@ -21,20 +30,24 @@ std::string CPos ::toStr ( long long int posInt ) {
         pos = digit + pos;
         posInt /= 26;
     }
-    std::cout << "decoded : " << pos << std::endl;
     return pos;
 }
+
+/*
+ * Constructor
+ * @param str - string
+ */
 
 CPos::CPos ( std::string_view str ) {
 
     m_fixedColumn = false;
     m_fixedRow = false;
     auto it = str.begin();
-    if ( * it == '$') {
+    if ( * it == '$') {//if the column is fixed
         m_fixedColumn = true;
         it++;//skipping
     }
-    while ( std::isalpha ( *it ) ) {
+    while ( std::isalpha ( *it ) ) {//while it's a letter
         m_str.push_back ( ::toupper( *it ) );
         it++;
     }
@@ -42,50 +55,84 @@ CPos::CPos ( std::string_view str ) {
     if ( it == str.begin() || it == str.end() )
         throw std::invalid_argument ( "format of position isn't right" );//if we haven't moved from the beginning means that first element isn't a letter
 
-    toInt ( m_str , m_int );
+    toInt ( m_str , m_int );//convert the string to a pair of integers
     auto it2 = it;
-    if ( * it == '$' ) {
+    if ( * it == '$' ) {//if the row is fixed
         m_fixedRow = true;
         it++;
     }
 
     std::string tmp;
-    while ( std::isdigit ( *it ) && it != str.end() ) {
+    while ( std::isdigit ( *it ) && it != str.end() ) {//while it's a digit
         m_str.push_back( *it );
         tmp.push_back( * it );
         it++;
     }
-    m_int.second = std::stoi( tmp );
+
+    m_int.second = std::stoi( tmp );//convert the row to an integer
 
     if ( it == it2 || it != str.end() )
         throw std::invalid_argument ( "format of position isn't right" );//if we haven't moved from the beginning
 
-
 }
+
+/*
+ * Get the position as a string
+ * @return the position as a string
+ */
 
 std::string CPos::getPosStr() const {
     return m_str;
 }
 
+/*
+ * Get the position as a pair of integers
+ * @return the position as a pair of integers
+ */
+
 std::pair<long long int , int> CPos::getPosInt() const {
     return m_int;
 }
+
+/*
+ * Check if the column is fixed
+ * @return true if the column is fixed, false otherwise
+ */
 
 bool CPos::isFixedColumn() const {
     return m_fixedColumn;
 }
 
+/*
+ * Check if the row is fixed
+ * @return true if the row is fixed, false otherwise
+ */
+
 bool CPos::isFixedRow() const {
     return m_fixedRow;
 }
 
+/*
+ * Set the fixed column
+ * @param toSet - true if the column should be fixed, false otherwise
+ */
 void CPos::setFixedColumn ( bool toSet ) {
     m_fixedColumn = toSet;
 }
 
+/*
+ * Set the fixed row
+ * @param toSet - true if the row should be fixed, false otherwise
+ */
+
 void CPos::setFixedRow ( bool toSet ) {
     m_fixedRow = toSet;
 }
+
+/*
+ * Serialize the object
+ * @param out - output stream
+ */
 
 void CPos::serialize ( std::ostream & out ) const {
     size_t size = m_str.size();
@@ -95,6 +142,12 @@ void CPos::serialize ( std::ostream & out ) const {
     out.write ( reinterpret_cast< const char * > ( & m_fixedRow ) , sizeof ( m_fixedRow ) );
     out.write ( reinterpret_cast< const char * > ( & m_int ) , sizeof ( m_int ) );
 }
+
+/*
+ * Deserialize the object
+ * @param in - input stream
+ * @return true if the object was successfully deserialized, false otherwise
+ */
 
 bool CPos::deSerialize ( std::istream & in ) {
     size_t size;
--- excel/CPos.h
+++ excel/CPos.h
@@ -7,15 +7,58 @@ class CPos {
 public:
     CPos() = default;
     CPos ( std::string_view str );
+    /*
+     * Convert a string to an integer
+     * @param pos - string
+     * @param posInt - pair of integers
+     */
     void toInt ( const std::string & pos , std::pair<long long int , int> & posInt );
+    /*
+     * Convert integer to a string
+     * @param posInt - integer
+     * @return the string
+     */
     std::string toStr ( long long int posInt );
+    /*
+     * Get the position as a string
+     * @return the string
+     */
     std::string  getPosStr () const;
+    /*
+     * Get the position as an integer
+     * @return the integer
+     */
     std::pair<long long int , int> getPosInt () const;
+    /*
+     * Check if the column is fixed
+     * @return true if the column is fixed, false otherwise
+     */
     bool isFixedColumn() const;
+    /*
+     * Check if the row is fixed
+     * @return true if the row is fixed, false otherwise
+     */
     bool isFixedRow() const;
+    /*
+     * Set the fixed column
+     * @param toSet - true if the column should be fixed, false otherwise
+     */
     void setFixedColumn ( bool toSet );
+    /*
+     * Set the fixed row
+     * @param toSet - true if the row should be fixed, false otherwise
+     */
     void setFixedRow ( bool toSet );
+    /*
+     * Serialize the object
+     * @param out - output stream
+     */
     void serialize ( std::ostream & out ) const;
+    /*
+     * Deserialize the object
+     * @param in - input stream
+     * @return true if the object was deserialized, false otherwise
+     */
     bool deSerialize ( std::istream & in );
 
 private:
@@ -24,6 +67,5 @@ private:
     bool m_fixedRow;
     std::pair< long long int , int> m_int;
 };
-
 
 #endif CPOS_H
